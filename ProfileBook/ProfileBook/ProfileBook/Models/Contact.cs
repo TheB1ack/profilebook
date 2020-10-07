4a353d8409db234c496b1bb77041880b8ae0270d
Alex <47815470+TheB1ack@users.noreply.github.com>
2020-10-07 15:25:49 +0300

    Created database

---
+++ ProfileBook/ProfileBook/ProfileBook/Models/Contact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfileBook.Models
+{
+    class Contact
+    {
+        private int contactId;
+        private int userId;
+        private string nickName;
+        private string fullName;
+        private DateTime addTime;
+
+        public int ContactId
+        {
+            get { return contactId; }
+            private set { contactId = value; }
+        }
+        public string NickName
+        {
+            get { return nickName; }
+            private set { nickName = value; }
+        }
+        public string FullName
+        {
+            get { return fullName; }
+            private set { fullName = value; }
+        }
+        public DateTime AddTime
+        {
+            get { return addTime; }
+            private set { addTime = value; }
+        }
+
+
+    }
+}
---
+++ ProfileBook/ProfileBook/ProfileBook/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProfileBook.Models
+{
+    [Table("Users")]
+    class User
+    {
+        
+        private int userId;
+        private string userLogin;
+        private List<Contact> listOfContacts;
+
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int UserId
+        {
+            get { return userId; }
+            set
+            {
+                userId = value;
+            }
+        }
+
+        [Ignore]
+        public List<Contact> ListOfContacts
+        {
+            get { return listOfContacts; }
+            set
+            {
+                listOfContacts = value;
+            }
+        } 
+
+    }
+}

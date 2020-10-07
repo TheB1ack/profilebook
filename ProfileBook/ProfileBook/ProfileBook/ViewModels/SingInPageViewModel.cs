1c2a57519a2acbb95f73c6b9d9c969d8a60791d2
Alex <47815470+TheB1ack@users.noreply.github.com>
2020-10-07 10:58:28 +0300

    SingUp and SingIn frontend  finish

--- ProfileBook/ProfileBook/ProfileBook/ViewModels/SingInPageViewModel.cs
+++ ProfileBook/ProfileBook/ProfileBook/ViewModels/SingInPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,49 @@ using Xamarin.Forms;
 
 namespace ProfileBook.ViewModels
 {
-    public class SingInPageViewModel : ViewModelBase
+    public class SingInPageViewModel : ViewModelBase, IInitialize
     {
+        IPageDialogService _pageDialog;
+        private string _loginField;
+        private string _passwordField;
+        public string LoginField
+        {
+            get { return _loginField; }
+            set
+            {
+                SetProperty(ref _loginField, value);
+            }
+        }
+        public string PasswordField
+        {
+            get { return _passwordField; }
+            set
+            {
+                SetProperty(ref _passwordField, value);
+            }
+        }
         public ICommand TapCommand => new Command<string>((url) => NavigationService.NavigateAsync(url));
 
-        public SingInPageViewModel(INavigationService navigationService) : base(navigationService)
+        public SingInPageViewModel(INavigationService navigationService, IPageDialogService pageDialog) : base(navigationService)
         {
             Title = "User SingIn";
+            _pageDialog = pageDialog;
+            //alert();
+        }
+
+        private void alert(string message = "alert")
+        {
+            _pageDialog.DisplayAlertAsync("", message, "OK");
+        }
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            alert();
+        }
+        public void Initialize(NavigationParameters parameters)
+        {
+            alert();
+            LoginField = (string)parameters["login"];
+            PasswordField = (string)parameters["password"];
         }
     }
 }
--- ProfileBook/ProfileBook/ProfileBook/ViewModels/SingUpPageViewModel.cs
+++ ProfileBook/ProfileBook/ProfileBook/ViewModels/SingUpPageViewModel.cs
@@ -7,54 +7,131 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
 namespace ProfileBook.ViewModels
 {
     public class SingUpPageViewModel : ViewModelBase
     {
-        private DelegateCommand SingUpButtonClick { get; }
+
         IPageDialogService _pageDialog;
         private string _loginField;
+        private string _passwordField;
+        private string _sPasswordField;
+        private bool _isButtonEnable;
         public string LoginField
         {
             get { return _loginField; }
             set
             {
-                _loginField = value;              
+                _loginField = value;
+                IsButtonEnable = TryActivateButton();
+            }
+        }
+        public string PasswordField
+        {
+            get { return _passwordField; }
+            set
+            {
+                _passwordField = value;
+                IsButtonEnable = TryActivateButton();
+            }
+        }
+        public string SPasswordField
+        {
+            get { return _sPasswordField; }
+            set
+            {
+                _sPasswordField = value;
+                IsButtonEnable = TryActivateButton();
+            }
+        }
+        public bool IsButtonEnable
+        {
+            get { return _isButtonEnable; }
+            set
+            {
+                SetProperty(ref _isButtonEnable, value);
             }
         }
 
-        //public event PropertyChangedEventHandler PropertyChanged;
-        //private void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        //{
-        //    var handler = PropertyChanged;
-        //    if (handler != null)
-        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        //}
+        public DelegateCommand SingUpBClick { get; }
+
         public SingUpPageViewModel(INavigationService navigationService, IPageDialogService pageDialog) : base(navigationService)
         {
+
             Title = "User SingUp";
             _pageDialog = pageDialog;
-            SingUpButtonClick = new DelegateCommand(VerifyAndSave);
+            SingUpBClick = new DelegateCommand(VerifyAndSave);
+            IsButtonEnable = false;
         }
-        
-
+       /* private void alert(string message = "alert")
+        {
+            _pageDialog.DisplayAlertAsync("", message, "OK");
+        }*/
+        private bool TryActivateButton()
+        {
+            if (_loginField == null || _loginField == "")
+            {
+                return false;
+            }
+            if(_passwordField == null || _passwordField == "")
+            {
+                return false;
+            }
+            if(_sPasswordField == null || _sPasswordField == "")
+            {
+                return false;
+            }
+            return true;
+        }
         private void VerifyAndSave()
         {
-            //Button button = (Button)sender;
-            //button.Text = "Нажато!";
-            //button.BackgroundColor = Color.Red;
-            _pageDialog.DisplayAlertAsync("Alert", "message", "Ok");
-            VerifyInput();
+            if (VerifyInput())
+            {
+                var parameters = new NavigationParameters();
+                parameters.Add("password", _passwordField);
+                parameters.Add("login", _loginField);
+                NavigationService.NavigateAsync("SingInPage", parameters);
+            }
             
         }
 
         private bool VerifyInput()
         {
-            //проверка логина с бд
-            if (_loginField != "")
+            if(_loginField.Length <=4 || _loginField.Length >= 16)
             {
+                _pageDialog.DisplayAlertAsync("", "Login  must be at least 4 and no more than 16!", "OK");
+                return false;
+            }
+            if(System.Text.RegularExpressions.Regex.IsMatch(_loginField[0].ToString(), @"^[0-9]+"))
+            {
+                _pageDialog.DisplayAlertAsync("", "Login musn't start with number!", "OK");
+                return false;
+            }
+            //
+            //проверка логина в бд
+            //
+            if (System.Text.RegularExpressions.Regex.IsMatch(_loginField[0].ToString(), @"^[0-9]+"))
+            {
+                
+                _pageDialog.DisplayAlertAsync("", "This login is already taken!Make it unique!", "OK");
+                return false;
+            }
+            if (_passwordField.Length <= 8 || _passwordField.Length >= 16)
+            {
+                _pageDialog.DisplayAlertAsync("", "Password must be at least 8 and no more than 16 and must contain at least one uppercase letter, one lowercase letter and one number!", "OK");
+                return false;
+            }
+            if (!System.Text.RegularExpressions.Regex.IsMatch(_passwordField, @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])"))
+            {
+                _pageDialog.DisplayAlertAsync("", "Password must be at least 8 and no more than 16 and must contain at least one uppercase letter, one lowercase letter and one number!", "OK");
+                return false;
+            }
+            if (_passwordField != _sPasswordField)
+            {
+                _pageDialog.DisplayAlertAsync("", "Passwords must match!", "OK");
                 return false;
             }
             return true;
